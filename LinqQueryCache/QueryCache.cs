36963f04fd2f1834b3ac2f9da3c7b63a95154183
Ricardo Peres <rjperes@hotmail.com>
2024-06-04 20:46:14 +0100

    Added comments

--- LinqQueryCache/QueryCache.cs
+++ LinqQueryCache/QueryCache.cs
@@ -5,9 +5,18 @@ namespace LinqQueryCache
 {
     public static class QueryCache
     {
+        /// <summary>
+        /// The cache to use.
+        /// </summary>
         public static IMemoryCache Default { get; set; } = new MemoryCache(Options.Create<MemoryCacheOptions>(new MemoryCacheOptions()));
 
+        /// <summary>
+        /// Raised when the cache is hit.
+        /// </summary>
         public static event Action<IQueryable>? Hit;
+        /// <summary>
+        /// Raised when the cache is missed.
+        /// </summary>
         public static event Action<IQueryable>? Miss;
 
         internal static void RaiseHit<T>(IQueryable<T> queryable)
--- LinqQueryCache/QueryableExtensions.cs
+++ LinqQueryCache/QueryableExtensions.cs
@@ -11,36 +11,105 @@ namespace LinqQueryCache
             return (queryable as IInfrastructure<IServiceProvider>)?.GetService<IMemoryCache>() ?? QueryCache.Default;
         }
 
+        /// <summary>
+        /// Returns a query from the cache.
+        /// </summary>
+        /// <typeparam name="T">The query type.</typeparam>
+        /// <param name="queryable">The query.</param>
+        /// <returns>The cached query.</returns>
+        /// <exception cref="InvalidOperationException">Throws an exception if the cache is not set.</exception>
         public static IQueryable<T> AsCacheable<T>(this IQueryable<T> queryable) where T : class
         {
+            ArgumentNullException.ThrowIfNull(queryable, nameof(queryable));
+
             var cache = GetCache<T>(queryable);
+
+            if (cache == null)
+            {
+                throw new InvalidOperationException("Cache is not set");
+            }
 
             var cachedQuery = new QueryableWrapper<T>(cache, queryable);
 
             return cachedQuery;
         }
 
+        /// <summary>
+        /// Returns and possibly adds a query to the cache.
+        /// </summary>
+        /// <typeparam name="T">The query type.</typeparam>
+        /// <param name="queryable">The query.</param>
+        /// <param name="duration">The cache duration.</param>
+        /// <returns>The cached query.</returns>
         public static IQueryable<T> AsCacheable<T>(this IQueryable<T> queryable, TimeSpan duration) where T : class
         {
+            ArgumentNullException.ThrowIfNull(queryable, nameof(queryable));
+         
             return AsCacheable(queryable, (int)duration.TotalSeconds);
         }
 
+        /// <summary>
+        /// Returns and possibly adds a query to the cache.
+        /// </summary>
+        /// <typeparam name="T">The query type.</typeparam>
+        /// <param name="queryable">The query.</param>
+        /// <param name="durationSeconds">The cache duration in seconds.</param>
+        /// <returns>The cached query.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Throws an exception if the duration is invalid.</exception>
+        /// <exception cref="InvalidOperationException">Throws an exception if the cache is not set.</exception>
         public static IQueryable<T> AsCacheable<T>(this IQueryable<T> queryable, int durationSeconds) where T : class
         {
+            ArgumentNullException.ThrowIfNull(queryable, nameof(queryable));
+
+            if (durationSeconds < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(durationSeconds));
+            }
+
             var cache = GetCache<T>(queryable);
+
+            if (cache == null)
+            {
+                throw new InvalidOperationException("Cache is not set");
+            }
 
             var cachedQuery = new QueryableWrapper<T>(cache, queryable, durationSeconds);
 
             return cachedQuery;
         }
 
+        /// <summary>
+        /// Returns and possibly adds a query to the cache.
+        /// </summary>
+        /// <typeparam name="T">The query type.</typeparam>
+        /// <param name="queryable">The query.</param>
+        /// <param name="duration">The cache duration.</param>
+        /// <returns>The cached query.</returns>
         public static IOrderedQueryable<T> AsCacheable<T>(this IOrderedQueryable<T> queryable, TimeSpan duration) where T : class
         {
+            ArgumentNullException.ThrowIfNull(queryable, nameof(queryable));
+
+
             return (IOrderedQueryable<T>)AsCacheable(queryable as IQueryable<T>, duration);
         }
 
+        /// <summary>
+        /// Returns and possibly adds a query to the cache.
+        /// </summary>
+        /// <typeparam name="T">The query type.</typeparam>
+        /// <param name="queryable">The query.</param>
+        /// <param name="durationSeconds">The cache duration in seconds.</param>
+        /// <returns>The cached query.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Throws an exception if the duration is invalid.</exception>
         public static IOrderedQueryable<T> AsCacheable<T>(this IOrderedQueryable<T> queryable, int durationSeconds) where T : class
         {
+            ArgumentNullException.ThrowIfNull(queryable, nameof(queryable));
+
+            if (durationSeconds < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(durationSeconds));
+            }
+
             return (IOrderedQueryable<T>)AsCacheable(queryable as IQueryable<T>, durationSeconds);
         }
     }
